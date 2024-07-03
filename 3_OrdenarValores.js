22913f4b32d0c8a8327cb429649c23c8220ae21a
Mirile2024 <mirileas@gmail.com>
2024-07-02 23:02:19 -0300

    ejercicio 3 sola

--- 3_OrdenarValores.js
+++ 3_OrdenarValores.js
@@ -1,1 +1,11 @@
 // 3. Ordenar en forma creciente tres valores diferentes A, B, C.
+
+const numeroA = 23
+const numeroB = 15
+const numeroC = 34
+// var arrayNumbers = [23, 15, 34]
+var arrayNumbers = [(numeroA),(numeroB),(numeroC)]
+// esta funcion hace que se ordenen bien
+console.log(arrayNumbers.sort(function (a, b){
+    return a - b;
+}))
